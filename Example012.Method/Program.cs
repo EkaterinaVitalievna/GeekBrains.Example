b316fbd3273396c46909dc70ffa1d1922a045325
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-16 00:20:58 +0300

    add new project

---
+++ Example012.Method/Program.cs
@@ -0,0 +1,55 @@
+﻿// 4 Группы методов
+// 1 группа методов
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+//Method1();//скобочки обязательны
+
+
+
+// 2 группа методов
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2("вводим нужный нам текст");
+//как вариант
+//Method2(msg: "вводим нужный и т.д.")
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21("Учиться полезно", 4);//текст в кавычках выводится 4 раза, цифру указываем сами
+//если мы указываем название аргумента, то можно их прописывать не соблюдая очерёдность:
+//Method21(count: 5, msg: "новый самый важный текст");
+
+int Method3()
+    {
+        return DateTime.Now.Year;
+    }
+
+int year = Method3();
+//Console.WriteLine(year);
+
+//Группа 4
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;//можно в кавычки заложить то, что нужно, мы прописали, что вначале выходит пустая строка
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(5, "крапиволистный колокольчик, ");
+// Console.WriteLine(res);
---
+++ Example13.For/Program.cs
@@ -0,0 +1,11 @@
+﻿string Method(int count, string text)
+{
+    string result = String.Empty;//можно в кавычки заложить то, что нужно, мы прописали, что вначале выходит пустая строка
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method(50, "любимая мама ");
+Console.WriteLine(res);
